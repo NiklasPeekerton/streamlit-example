0a489ea60373ae495c244a5ef5bde039b9c93c38
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2021-11-16 12:04:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,14 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# überStocks
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
 """
-# My first app
-Here's our first attempt at using data to create a table:
+# Dow Jones PE values
+These are stocks listed on Dow Jones sorted by PE:
 """
 
 import pandas as pd
@@ -27,4 +21,31 @@ df = pd.DataFrame({
   'second column': [10, 20, 30, 40]
 })
 
-df
+from yahoo_fin import stock_info as si
+
+# get list of Dow tickers
+dow_list = si.tickers_dow()
+#sp_list = si.tickers_sp500()
+ 
+# et data in the current column for each stock's valuation table
+dow_stats = {}
+for ticker in dow_list:
+    temp = si.get_stats_valuation(ticker)
+    temp = temp.iloc[:,:2]
+    temp.columns = ["Attribute", "Recent"]
+ 
+    dow_stats[ticker] = temp
+ 
+ 
+# combine all the stats valuation tables into a single data frame
+combined_stats = pd.concat(dow_stats)
+combined_stats = combined_stats.reset_index()
+ 
+del combined_stats["level_1"]
+ 
+# update column names
+combined_stats.columns = ["Ticker", "Attribute", "Recent"]
+
+# get P/E ratio for each stock
+dow_pe = combined_stats[combined_stats.Attribute.str.contains("Trailing P/E")]
+dow_pe

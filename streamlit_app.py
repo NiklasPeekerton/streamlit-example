5134e997e4e4e066d38c29f7d6e3ca08504f3708
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2021-11-18 18:54:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,28 +52,21 @@ dow_pe
 These are stocks listed on S&P 500 with their NCAV:
 """
 
-sp_list = si.tickers_sp500()
+
 dow_list = si.tickers_dow()
 
 sp_stats = {} 
-BalanceSheets = {}
-
-allstats = []
-
-for ticker in dow_list:
-    temp = si.get_quote_data(ticker)
-    sheet = si.get_balance_sheet(ticker)
-    sp_stats[ticker] = temp
-    BalanceSheets[ticker] = sheet
 
 
-    try:
-        allstats.append([ticker, BalanceSheets[ticker].loc['totalLiab'][0], BalanceSheets[ticker].loc['totalCurrentAssets'][0],
-                        sp_stats[ticker]['regularMarketPrice'], sp_stats[ticker]['sharesOutstanding']])
-    except:
-        print(ticker)
+@st.cache
+def get_data():
+    for ticker in dow_list:
+        temp = si.get_quote_data(ticker)
+        sp_stats[ticker] = temp
+     
+    return sp_stats
 
+df = get_data()
+df
 
-allstatsdf = pd.DataFrame(allstats, columns=['Ticker','TotalLiabilities','CurrentAssets','Market Price', 'Shares Outstanding'])
-allstatsdf
 

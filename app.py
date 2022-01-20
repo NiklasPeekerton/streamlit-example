dd4c1d7053e40495cdc4e28358d5ab84d0e438a2
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-01-20 16:50:05 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import (
+    home,
+  sp,
+  dj
+)
+
+st.set_page_config(layout="wide")
+
+
+apps = MultiApp()
+
+# Add all your application here
+
+apps.add_app("Home", home.app)
+apps.add_app("S&P", sp.app)
+apps.add_app("Dow Jones", dj.app)
+
+
+# The main app
+apps.run()

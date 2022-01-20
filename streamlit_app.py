ea1a00dc3b72dc5cf1b0a6205549637f3898a608
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-01-20 15:59:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,19 @@ import streamlit as st
 from datetime import datetime
 import pandas as pd
 import numpy as np
+import streamlit as st
+from streamlit_option_menu import option_menu
 
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
+# horizontal menu
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
 
 
 sp_list = si.tickers_sp500()

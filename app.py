2f14dc2d54d610ddb3ae4c19be3b0b330c56e8e3
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-01-20 17:23:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import (
-    home,
-  sp,
-  dj
-)
+from apps import home, sp
 
 
 PAGES = {
@@ -25,7 +21,7 @@ apps = MultiApp()
 
 apps.add_app("Home", home.app)
 apps.add_app("S&P", sp.app)
-apps.add_app("Dow Jones", dj.app)
+#apps.add_app("Dow Jones", dj.app)
 
 
 # The main app

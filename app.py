5e60d0f68b9848b2c7f4c87c26ec67f19da423bd
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-01-20 17:26:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,7 @@ from multiapp import MultiApp
 from apps import home, sp
 
 
-PAGES = {
-    "Home": home,
-    "SP 500": sp
-}
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+
 
 st.set_page_config(layout="wide")
 
@@ -26,3 +19,12 @@ apps.add_app("S&P", sp.app)
 
 # The main app
 apps.run()
+
+PAGES = {
+    "Home": home,
+    "SP 500": sp
+}
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()

7a124c1a5d1afd5cd5b2aba7510c8839de99bf5f
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-01-21 12:15:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from multiapp import MultiApp
 from apps import home
 from apps import sp
 from apps import dj
+from apps import data_test
 
 
 
@@ -17,6 +18,7 @@ apps = MultiApp()
 apps.add_app("Home", home.app)
 apps.add_app("S&P", sp.app)
 apps.add_app("Dow Jones", dj.app)
+apps.add_app("Data test", data.app)
 
 
 # The main app

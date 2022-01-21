a23b0716456be8bda110bccbcf4c6b36dbecce39
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-01-21 15:07:10 +0100

    Update data_test.py

--- apps/data_test.py
+++ apps/data_test.py
@@ -4,13 +4,46 @@ import pandas as pd
 from data.data import fetch_data
 
 def app():
-    st.title('Data Stats')
+    st.title('Testing data fetch/read')
 
-    st.write("This is a sample data stats in the mutliapp.")
-    st.write("See `apps/data_stats.py` to know how to use it.")
+    st.write("Should contain data from 2 stocks at the moment")
 
     st.markdown("### Plot Data")
-    df = fetch_data()
+    #df = fetch_data()
 
     #st.line_chart(df)
-    df
+    clicky = st.button('Fetch data')
+
+    if clicky:
+        fetch_data()
+        st.write('Downloading data')
+    with open('Financials.pkl','rb') as read_file:
+        Financialsj = pickle.load(read_file)
+
+    with open('Quote.pkl','rb') as read_file:
+        Quotej = pickle.load(read_file)
+
+    with open('Dividends.pkl','rb') as read_file:
+        Dividendsj = pickle.load(read_file)
+
+    with open('Earnings.pkl','rb') as read_file:
+        Earningsj = pickle.load(read_file)
+
+    with open('Price.pkl','rb') as read_file:
+        Pricej = pickle.load(read_file)
+
+    keysIS = ['totalRevenue', 'netIncome', 'interestExpense']
+    keysBS = ['totalLiab', 'totalCurrentAssets', 'totalCurrentLiabilities', 'longTermDebt', 'totalStockholderEquity', 'intangibleAssets', 'totalAssets']
+    keysQ = ['longName', 'regularMarketPrice', 'trailingPE', 'sharesOutstanding', 'fiftyTwoWeekRange', 'epsTrailingTwelveMonths', 'bookValue', 'priceToBook', 'trailingAnnualDividendRate', 'trailingAnnualDividendYield']
+
+    haba = []
+
+    yearnow = pd.Timestamp.now().year
+    for ticker in dow_list:
+        try:
+            IncomeStatement = Financialsj[ticker]['yearly_income_statement']
+            ist = IncomeStatement.reindex(keysIS)
+            BalanceSheet = Financialsj[ticker]['yearly_balance_sheet']
+            bs = BalanceSheet.reindex(keysBS)
+            TR = ist.loc['totalRevenue'][0]
+    TR

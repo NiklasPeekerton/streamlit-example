7be7732eb1c6e2c3496e3199c426d8f639259c35
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-01-24 12:47:34 +0100

    Update data_test.py

--- apps/data_test.py
+++ apps/data_test.py
@@ -10,6 +10,46 @@ minilist = ['A', 'ACN']
 
 
 def app():
+    Financials = {}
+    Quote = {}
+    Dividends = {}
+    Earnings = {}
+    Price = {}
+
+    #Fetches data. Cache somehow?
+
+    def fetch_data():
+        for ticker in tqdm(minilist):
+            try:
+                fin = si.get_financials(ticker, yearly=True, quarterly=False)
+                qut = si.get_quote_data(ticker)
+                div = si.get_dividends(ticker)
+                earn = si.get_earnings_history(ticker)
+                price = si.get_data(ticker,interval='1mo')
+                Financials[ticker] = fin
+                Quote[ticker] = qut
+                Dividends[ticker] = div
+                Earnings[ticker] = earn
+                Price[ticker] = price
+            except Exception as e:
+                print(ticker, e, 'contains sum bullshit')
+
+        with open('Financials.pkl', 'wb') as f:
+            pickle.dump(Financials, f)
+
+        with open('Quote.pkl', 'wb') as f:
+            pickle.dump(Quote, f)
+
+        with open('Dividends.pkl', 'wb') as f:
+            pickle.dump(Dividends, f)
+
+        with open('Earnings.pkl', 'wb') as f:
+            pickle.dump(Earnings, f)
+
+        with open('Price.pkl', 'wb') as f:
+            pickle.dump(Price, f)
+
+        return()
     st.title('Testing data fetch/read')
 
     st.write("Should contain data from 2 stocks at the moment")

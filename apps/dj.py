8b833e616f82570ab00a64f5724ddd123e8570e7
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-04-11 19:59:11 +0200

    Update dj.py

--- apps/dj.py
+++ apps/dj.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
-from data.data import fetch_data
+from data.data import read_data
 
 
 #with st.sidebar:

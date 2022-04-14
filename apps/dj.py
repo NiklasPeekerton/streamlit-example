fb99edf177cc75cbb2b1d68384ef1ba44b032a3c
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-04-14 11:01:15 +0200

    Update dj.py

--- apps/dj.py
+++ apps/dj.py
@@ -45,7 +45,7 @@ def app():
     st.header('Overall scores')
     st.subheader('Overall score')
     overall = newdf[['Ticker','Name','Overall score']].sort_values(by=['Overall score'], ascending=False)
-    overall1 = overall.set_index('Overall score')
+    #overall1 = overall.set_index('Overall score')
     st.bar_chart(overall1[['Name','Overall score']])
     st.dataframe(overall)
 

068c310a5d2ea122ff1d53537186a19fb1c345d7
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-04-14 13:18:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from apps import sp
 from apps import dj
 from apps import data_test
 from apps import Russell3000
+from apps import nasdaq
+from apps import ftse100
+from apps import ftse250
+from apps import nifty50
+from apps import niftybank
 
 
 
@@ -19,6 +24,11 @@ apps.add_app("Home", home.app)
 apps.add_app("S&P", sp.app)
 apps.add_app("Dow Jones", dj.app)
 apps.add_app("Russell3000", Russell3000.app)
+apps.add_app("Nasdaq", nasdaq.app)
+apps.add_app("FTSE 100", ftse100.app)
+apps.add_app("FTSE 250", ftse250.app)
+apps.add_app("NIFTY 50", nifty50.app)
+apps.add_app("NIFTY bank", niftybank.app)
 apps.add_app("Data test", data_test.app)
 
 

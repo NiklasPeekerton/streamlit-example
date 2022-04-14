69d9fd7f22cea7576e423e914b3f33be5344cacc
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-04-14 12:57:12 +0200

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -23,8 +23,12 @@ def app():
     #st.write("This is a sample home page in the mutliapp.")
     #st.write("See `apps/home.py` to know how to use it.")
     #st.bar_chart(data=None, width=0, height=0, use_container_width=True)
+    dividend = 'https://drive.google.com/file/d/150UDrwhVd3hH0nwU1Y0IptwlYIu5akR4/view?usp=sharing'
+    divpath = 'https://drive.google.com/uc?export=download&id='+dividend.split('/')[-2]
+    divdict = pd.read_pickle(divpath)
+    Dividendslist = list(divdict.keys())
 
-    newdf = read_data(dow_list)
+    newdf = read_data(Dividendslist)
     overall = newdf[['Ticker','Name','Overall score']].sort_values(by=['Overall score'], ascending=False)
     c = alt.Chart(overall).mark_circle().encode(
      x='Name', y='Overall score', size='Overall score', color='Overall score', tooltip=['Name', 'Overall score'])

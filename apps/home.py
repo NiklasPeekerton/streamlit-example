2ff0933c233c1d5de6dbf8cd688eb0144afcc140
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-04-14 10:17:39 +0200

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,15 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from apps import dj
+from yahoo_fin import stock_info as si
+from tqdm.notebook import trange, tqdm
+import pickle
+import streamlit as st
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import streamlit as st
+from streamlit_option_menu import option_menu
+from data.data import read_data
 
 def app():
-    st.title('Home')
+    st.title('Stonkotracker 3000')
 
-    st.write("This is a sample home page in the mutliapp.")
-    st.write("See `apps/home.py` to know how to use it.")
+    #st.write("This is a sample home page in the mutliapp.")
+    #st.write("See `apps/home.py` to know how to use it.")
+    #st.bar_chart(data=None, width=0, height=0, use_container_width=True)
 
-    st.markdown("### Sample Data")
-    #df = create_table()
-    #st.write(df)
-
-    st.write('Navigate to `Data Stats` page to visualize the data')
+    newdf = read_data(dow_list)
+    st.bar_chart(newdf)
